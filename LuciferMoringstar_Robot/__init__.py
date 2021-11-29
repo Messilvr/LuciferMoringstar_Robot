2e3aca2367c8a761f577a68ea4abcc122ed4fa75
Messilvr <90956207+Messilvr@users.noreply.github.com>
2021-11-29 11:54:31 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -11,37 +11,23 @@ from .Channel import (
 )
 
 HELP = """
-<b><u>Basic Commads</u></b>
+Just add me to your group as admin. I will provide toons there!
+
+<b><u>Commads</u></b>
 
 ➥ /start : check if am alive or dead
 ➥ /about : About Me
-➥ /stats user Status
-
-<b><u>Bot Owner Only</u></b>
-
-➥ /broadcast Replay Any Message or Media
-➥ /ban_user  click ban_user more info
-➥ /unban_user click unban_user more info
-➥ /banned_users Banned User Details
-➥ /total How Many Files Added In Database
-➥ /logger  Get Logs
-➥ /delete Delete File From Database
-
-<b>[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
-
-@Mo_Tech_YT></b>
+➥ /Help  : To get some help
 """
 
 ABOUT = """
-➥ Name : Auto Filter With IMDB
+➥ Name : Mickey Mouse
     
-➥ Credits : Subin
+➥ Owner : [Disney](t.me/free_music123)
 
-➥ Editing : [Muhammed RK](t.me/Mrk_YT)
+➥ Creator : [Walt Disney](t.me/Geronimo1234)
 
-➥ Language : Python3
+➥ Workplace : [Disneyland](https://t.me/free_cartoons)
 
-➥ Library : Pyrogram Asyncio 1.13.0
-
-➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+➥ Birthday : [29/11/2019](https://www.google.com/search?q=steamboat+willie)
 """
